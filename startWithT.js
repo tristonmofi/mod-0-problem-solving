f312ede8865370cda80e196ca42f018a080380b5
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-04-05 17:52:20 -0600

    add correct syntax

---
+++ startWithT.js
@@ -0,0 +1,8 @@
+//Given an array of strings, return only the words that begin with the letter "t"
+
+var words = ["super", "tasteful", "guitar", "truck", "time", "plant"]
+
+for (var i = 0; i < words.length; i++) {
+
+console.log(words[i].startsWith("t"));
+}
