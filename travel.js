3ffd7830203bfac491aed61216f983be96cc2426
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-04-05 19:46:51 -0600

    add final syntax

---
+++ travel.js
@@ -0,0 +1,7 @@
+var travel = ["Italy", "Spain", "Greece", "Belize"]
+
+travel.sort();
+
+for (var i = 0; i < travel.length; i++) {
+    console.log(`The next place I want to go is ${travel[i]}!`)
+}
