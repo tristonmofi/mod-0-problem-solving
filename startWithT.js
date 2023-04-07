403522294e46a7f34924632d0ab269515b24522d
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-04-06 21:18:07 -0600

    Add corrections

--- startWithT.js
+++ startWithT.js
@@ -1,8 +1,27 @@
 //Given an array of strings, return only the words that begin with the letter "t"
 
+//Goal: 
+// - Create an array of strings
+// - Create a function with a parameter
+// - Make another variable and set it's value as `[]` to represent a new blank array
+// - Begin to iterate through the original array looking for words that start with 't' with an `if` statement
+// - The `startsWith()` method is calling on the parameter. The loop will find words that start with 't'
+// - If the return value of the `if` statement is true push the data to the new array with `.push()` method
+// - Set the `return` value as your new array
+// - Print to the console the function name and the argument
+
 var words = ["super", "tasteful", "guitar", "truck", "time", "plant"]
 
-for (var i = 0; i < words.length; i++) {
+function returnTWords(arr) {
 
-console.log(words[i].startsWith("t"));
+    var newArr = [];
+
+    for (var i = 0; i < words.length; i++) {
+        if (arr[i].startsWith('t') === true){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr
 }
+
+console.log(returnTWords(words));
